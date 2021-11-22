a29b3216f46f33f1b02431ed18e8c9c8009d8f54
Guilherme Henrique <gui_lirasilva@outlook.com>
2021-11-22 11:54:01 -0300

    Correcao de codigo

--- src/main/java/br/com/alura/carteiraAPI/modelo/Transacao.java
+++ src/main/java/br/com/alura/carteiraAPI/modelo/Transacao.java
@@ -9,6 +9,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
@@ -41,6 +42,7 @@ public class Transacao {
 	private TipoTransacao tipo;
 	
 	@ManyToOne
+	@JoinColumn(name = "usuario_id")
 	private Usuario usuario;
 
 	public Transacao(String ticker, BigDecimal preco, Integer quantidade, LocalDate data, TipoTransacao tipo,
